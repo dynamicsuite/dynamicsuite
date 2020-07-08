4756cf92661eba1442c4eacf38b9ba5817bb9d54
Grant Martin <commgdog@gmail.com>
2020-07-08 14:44:32 -0700

    Remove STDIN check for caching

--- scripts/create_environment.php
+++ scripts/create_environment.php
@@ -24,7 +24,7 @@ namespace DynamicSuite;
 define('DS_START', microtime(true));
 define('DS_VERSION', '7.0.0');
 define('DS_ROOT_DIR', realpath(__DIR__ . '/..'));
-define('DS_CACHING', false && !defined('STDIN'));
+define('DS_CACHING', false);
 ini_set('display_errors', 0);
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
