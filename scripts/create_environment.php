691145c581aad7efe138cb3c32d8a16d61e0ed1e
Grant Martin <commgdog@gmail.com>
2020-12-17 17:29:17 -0800

    Version increment

--- scripts/create_environment.php
+++ scripts/create_environment.php
@@ -22,7 +22,7 @@
 namespace DynamicSuite;
 
 define('DS_START', microtime(true));
-define('DS_VERSION', '8.0.1');
+define('DS_VERSION', '8.1.0');
 define('DS_ROOT_DIR', realpath(__DIR__ . '/..'));
 define('DS_CACHING', false);
 ini_set('display_errors', 0);
