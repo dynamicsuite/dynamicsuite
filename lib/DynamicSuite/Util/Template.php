0206e182f5dd7b05471a84654ccc21432d6a2201
Grant Martin <commgdog@gmail.com>
2021-04-09 07:31:17 -0700

    Update template

--- lib/DynamicSuite/Util/Template.php
+++ lib/DynamicSuite/Util/Template.php
@@ -1,23 +1,14 @@
 <?php
-/*
- * Dynamic Suite
- * Copyright (C) 2020 Dynamic Suite Team
+/**
+ * This file is part of the Dynamic Suite framework.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation version 3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * @package DynamicSuite\Util
+ * @author Grant Martin <commgdog@gmail.com>
+ * @copyright 2021 Dynamic Suite Team
  */
-
-/** @noinspection PhpUnused */
 
 namespace DynamicSuite\Util;
 
@@ -31,45 +22,13 @@ final class Template
 {
 
     /**
-     * Contents of the template.
-     *
-     * @var string
-     */
-    public string $contents;
-
-    /**
      * Template constructor.
      *
-     * @param string $contents
      * @return void
      */
-    public function __construct(string $contents = '')
+    public function __construct(public string $contents = '')
     {
         $this->contents = preg_replace('~>\s+<~', '><', preg_replace('/\s+/', ' ', $contents));
-    }
-
-    /**
-     * Prepend content to the template.
-     *
-     * @param string $content
-     * @return Template
-     */
-    public function prepend(string $content): Template
-    {
-        $this->contents = $content . $this->contents;
-        return $this;
-    }
-
-    /**
-     * Append content to the template.
-     *
-     * @param string $content
-     * @return Template
-     */
-    public function append(string $content): Template
-    {
-        $this->contents = $this->contents . $content;
-        return $this;
     }
 
     /**
